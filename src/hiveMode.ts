83e8df675f0e7c2c93e05a0fbf6a1ce58424a7cb
lleohao <lleohao@hotmail.com>
2018-09-05 14:28:29 +0800

    feat: Remove validation

--- src/hiveMode.ts
+++ src/hiveMode.ts
@@ -12,17 +12,18 @@ import Promise = monaco.Promise;
 import Uri = monaco.Uri;
 
 export function setupMode(defaults: LanguageServiceDefaultsImpl): void {
-  const client = new WorkerManager(defaults);
-  
-  const worker = (first: Uri, ...more: Uri[]): Promise<HiveWorker> => {
-    return client.getLanguageServiceWorker(...[first].concat(more));
-  };
-  
-  let languageId = defaults.languageId;
-  
-  monaco.languages.registerCompletionItemProvider(
-    languageId,
-    new languageFeatures.CompletionAdapter(worker)
-  );
-  new languageFeatures.DiagnosticsAdapter(languageId, worker, defaults);
+    const client = new WorkerManager(defaults);
+
+    const worker = (first: Uri, ...more: Uri[]): Promise<HiveWorker> => {
+        return client.getLanguageServiceWorker(...[first].concat(more));
+    };
+
+    let languageId = defaults.languageId;
+
+    monaco.languages.registerCompletionItemProvider(
+        languageId,
+        new languageFeatures.CompletionAdapter(worker)
+    );
+
+    // new languageFeatures.DiagnosticsAdapter(languageId, worker, defaults);
 }
